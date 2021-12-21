9022f0f749fd0f0ad5a1d9874a1af87b7509e4d1
marcelovicentegc <marcelovicentegc@gmail.com>
2021-12-21 12:45:45 -0300

    chore: add cors headers

--- api/render.ts
+++ api/render.ts
@@ -64,6 +64,8 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     }
 
     res.setHeader("Content-Type", "application/json");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
     res.status(200).json({
       html,
     });
